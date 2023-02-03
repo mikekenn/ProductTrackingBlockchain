0eaa5e3ac1056bb7d75627204b248adcde997e39
Michael Kennedy <michaelkennedy_228@hotmail.com>
2023-02-03 13:13:10 -0400

    add intial code for Product Registration Contract

---
+++ contracts/PRC.sol
@@ -0,0 +1,63 @@
+pragma solidity ^ 0.4 .10;
+
+contract PRC {
+
+  // Mapping from user address to boolean type
+  //mapping(address => bool) isAuthorized;
+
+  // Define struct
+  struct product {
+    TODO
+    //Team: To Create attributs/Define structure of product.
+  }
+
+  // As a prerequisite for some functions
+  //modifier onlyAdmin {
+  //}
+
+  //modifier onlyAuthorized(address addr) {
+  //}
+
+  // Constructor function
+  //constructor() public {
+  //}
+
+  // Register product information
+  //function productRegister(string productName, string productCode, string rawMaterials, address BACAddress) public onlyAuthorized(msg.sender) {
+  //}
+
+  // Register raw material information
+  //function materialRegister(string materialName, string materialCode, address BACAddress) public onlyAuthorized(msg.sender) {
+  //}
+
+  // Get the number of products
+  //function getNumberOfProducts() constant public returns(uint numberOfProducts) {
+  //}
+
+  // Get product information by id
+  //function getProductOfId(uint id) constant public returns(string productName, string productCode, string rawMaterials, address productOwner, uint timestamp, address BACAddress) {
+  //}
+
+  //function getIdOfCode(string productCode) constant public returns(uint id) {
+  //}
+
+  //function getAddressOfCode(string productCode) constant public returns(address addr) {
+  //}
+
+  // Add user to authorization list
+  //function addUser(address addr) public onlyAdmin {
+  //}
+
+  // Remove user
+  //function removeUser(address addr) public onlyAdmin {
+  //}
+
+  // Check if the user is authorized
+  //function checkUser(address addr) constant public returns(bool result) {
+  //}
+
+  // Destroy the contract
+  //function deleteContract() public onlyAdmin {
+  //}
+
+}
