6f0a7db914881fcb1835cf99574528b6b855e006
Michael Kennedy <michaelkennedy_228@hotmail.com>
2023-02-03 13:06:56 -0400

    Add initial code structure to be defined by team.

---
+++ contracts/BAC.sol
@@ -0,0 +1,56 @@
+pragma solidity ^ 0.4 .10;
+
+contract BAC {
+
+  // Mapping from user address to boolean type
+  //mapping(address => bool) isAuthorized;
+
+  // Define struct
+  struct batch {
+    TODO
+    //Team: To Create attributs/Define structure of batch.
+  }
+
+  // As a prerequisite for some functions
+  //modifier onlyAdmin {
+  //}
+
+  //modifier onlyAuthorized(address addr) {
+  //}
+
+  // Constructor function
+  //constructor() public {
+  //}
+
+  // Add production batch information
+  //function addBatch(string productBatch, string materialBatch, address TUCAddress) public onlyAuthorized(msg.sender) {
+  //}
+
+  // Get batch information by id
+  //function getBatchOfId(uint id) constant public returns(string productBatch, string materialBatch, address batchManager, address TUCAddress, uint addTime) {
+  //}
+
+  // Get the number of batches
+  //function getNumberOfBatchs() constant public returns(uint numberOfBatchs) {
+  //}
+
+  //function getAddressOfBatch(string productBatch) constant public returns(address addr) {
+  //}
+
+  // Add user to authorization list
+  //function addUser(address addr) public onlyAdmin {
+  //}
+
+  // Remove user
+  //function removeUser(address addr) public onlyAdmin {
+  //}
+
+  // Check if the user is authorized
+  //function checkUser(address addr) constant public returns(bool result) {
+  //}
+
+  // Destroy the contract
+  //function deleteContract() onlyAdmin public {
+  //}
+
+}
